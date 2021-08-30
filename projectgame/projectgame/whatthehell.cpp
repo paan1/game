309a252ab7d056f4e5b4128617c859ca93a6a27c
patiphan01 <86480648+paan1@users.noreply.github.com>
2021-08-30 15:10:52 +0700

    .

---
+++ projectgame/projectgame/whatthehell.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<windows.h>
+#include<conio.h>
+void draw_ship(int x, int y)
+{
+	COORD c = { x, y };
+	SetConsoleCursorPosition(
+		GetStdHandle(STD_OUTPUT_HANDLE), c);
+	printf(" <-0-> ");
+}
+int main()
+{
+	char ch = ' ';
+	int x = 38, y = 20;
+	draw_ship(x, y);
+	do {
+		if (_kbhit()) {
+			ch = _getch();
+			if (ch == 'a') { draw_ship(--x, y); }
+			if (ch == 'd') { draw_ship(++x, y); }
+			if (ch == 'w') { draw_ship(x, --y); }
+			if (ch == 's') { draw_ship(x, ++y); }
+			fflush(stdin);
+		}
+		Sleep(1);
+	} while (ch != 'x');
+	return 0;
+}
